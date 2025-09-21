f0ad8ca0949021a22cc54d3a5c143a0b0df2eb17
Eslam Linux <linuxeslam@gmail.com>
2025-09-22 00:29:20 +0300

    Update DependInfo.cmake

--- build/DesktopAlarm.dir/DependInfo.cmake
+++ build/DesktopAlarm.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/media/eslam/88C20D18C20D0C5E/my projects/clock/0.5/main.cpp" "CMakeFiles/DesktopAlarm.dir/main.cpp.o" "gcc" "CMakeFiles/DesktopAlarm.dir/main.cpp.o.d"
+  "/main.cpp" "CMakeFiles/DesktopAlarm.dir/main.cpp.o" "gcc" "CMakeFiles/DesktopAlarm.dir/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
