b8cbbaf06d51e49294eb12a5ab663a8f64229b93
Eslam Linux <linuxeslam@gmail.com>
2025-09-22 00:29:57 +0300

    Create cmake_clean.cmake

---
+++ build/DesktopAlarm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DesktopAlarm.dir/main.cpp.o"
+  "CMakeFiles/DesktopAlarm.dir/main.cpp.o.d"
+  "DesktopAlarm"
+  "DesktopAlarm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DesktopAlarm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
